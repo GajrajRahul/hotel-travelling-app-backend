f7b5aee267a0d9d1c937994c0b0188e19e4e04d0
GajrajRahul <rahulgajraj3497@gmail.com>
2024-12-13 15:56:29 +0530

    cors changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import partnerRouter from "./routes/partner.route.js";
 import adminRouter from "./routes/admin.route.js";
@@ -9,11 +10,19 @@ import HttpException from "./utils/HttpException.utils.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 // connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: ["https://crm.adventurerichaholidays.com"], // Add your frontend domain
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/admin", adminRouter);
 app.use("/partner", partnerRouter);
