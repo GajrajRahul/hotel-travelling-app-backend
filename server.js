518a5356c5fd2b6da9a216c49ec9f76bffed8f77
GajrajRahul <rahulgajraj3497@gmail.com>
2025-01-07 00:59:49 +0530

    pdf isse fix

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 // connectDB();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // app.use(
 //   cors({
